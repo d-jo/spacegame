09da7e972f445b846dfdc9f46052e5d16d97ec20
djo <declan.l.johnson@gmail.com>
2018-02-05 15:19:16 -0600

    Create entity.py, move code

--- entity.py
+++ entity.py
@@ -1,72 +1,6 @@
+import spacegame
 import pygame
 import util
-import entity
-import os, sys
-from pygame.locals import *
-import numpy as np
-from enum import Enum
-
-
-print("font,", (None != pygame.font))
-print("audio,", (None != pygame.mixer))
-
-class GameInit:
-
-    def __init__(self, width=640, height=480):
-        pygame.init()
-        pygame.time.Clock().tick(60)
-        self.dimensions = (width, height)
-        self.screen = pygame.display.set_mode(self.dimensions)
-        self.all_sprites = pygame.sprite.Group()
-        self.MOVETICK = pygame.USEREVENT + 1
-        self.CREATE_ENTITY = pygame.USEREVENT + 2
-        self.font = pygame.font.SysFont("Hack", 12)
-        self.player = pygame.sprite.Group()
-        self.all_sprites.add(ShipBase({"create_entity": self.CREATE_ENTITY}))
-        pygame.time.set_timer(self.MOVETICK, 500)
-    
-    def handle_events(self, events):
-        for event in events:
-            if event.type == self.MOVETICK:
-                for entity in self.all_sprites:
-                    entity.move()
-            if event.type == self.CREATE_ENTITY:
-                self.all_sprites.add(event.entity)
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            
-    def tick(self):
-        num = 0
-        while 1:
-            inputs = pygame.key.get_pressed()
-            self.handle_events(pygame.event.get())
-            self.screen.fill((0,0,0))
-            for entity in self.all_sprites:
-                entity.update(inputs, num)
-                self.screen.blit(entity.surf, entity.rect)
-                self.screen.blit(self.font.render(entity.text, True, entity.color), (entity.rect[0] + 2, entity.rect[1]))
-            pygame.display.flip()
-            num = num + 1
-
-
-class ShipType(Enum):
-    CIV = 0
-    CARGO = 1
-    DESTROYER = 2
-    FLEET = 3
-
-class Force:
-    
-    # IN RADIANS!!!
-    # REMEMBER THIS!!
-    def __init__(self, xmagnitude, ymagnitude):
-        self.xmagnitude = xmagnitude
-        self.ymagnitude = ymagnitude
-
-    def get_x_y(self):
-        return np.array([self.xmagnitude, self.ymagnitude])
-
 
 class SpaceEntity(pygame.sprite.Sprite):
     
@@ -143,10 +77,3 @@ class ShipBase(SpaceEntity):
                 pygame.event.post(event)
                 self.energy = 0
             self.energy += 4
-
-
-
-
-if __name__ == "__main__":
-    window = GameInit()
-    window.tick()
